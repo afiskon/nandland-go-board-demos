43ab1b137768cfc7bfb3d85a291c6297f5cad9f9
Aleksander Alekseev <afiskon@gmail.com>
2018-03-10 20:22:34 +0300

    Move list of .v files to the .yosys script

--- demos/pong/make.sh
+++ demos/pong/make.sh
@@ -4,8 +4,7 @@ set -e
 
 ./clean.sh
 
-# TODO: create build.yosys file
-yosys -p "read_verilog main.v; read_verilog VGA_Sync_Porch.v; read_verilog VGA_Sync_Pulses.v; read_verilog Sync_To_Count.v; read_verilog Debounce_Switch.v; read_verilog Pong_Top.v; read_verilog Pong_Paddle_Ctrl.v; read_verilog Pong_Ball_Ctrl.v; synth_ice40 -blif main.blif"
+yosys < make.yosys
 arachne-pnr -d 1k -P vq100 -p main.pcf main.blif -o main.asc
 icetime -c 40 main.asc
 icepack main.asc main.bin
