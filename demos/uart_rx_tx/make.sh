9b7f36fb884075bc932eda9b6266072471e1763b
Aleksander Alekseev <afiskon@gmail.com>
2018-03-10 20:30:51 +0300

    Move list of .v file to .yosys script

--- demos/uart_rx_tx/make.sh
+++ demos/uart_rx_tx/make.sh
@@ -4,7 +4,7 @@ set -e
 
 ./clean.sh
 
-yosys -p "read_verilog UART_RX.v; read_verilog UART_TX.v; read_verilog Binary_To_7Segment.v; read_verilog main.v; synth_ice40 -blif main.blif"
+yosys < build.yosys
 arachne-pnr -d 1k -P vq100 -p main.pcf main.blif -o main.asc
 icetime -c 40 main.asc
 icepack main.asc main.bin
