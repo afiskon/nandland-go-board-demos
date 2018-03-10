0aec95c4671cc3c1d681f35d93deb36ca8f40760
Aleksander Alekseev <afiskon@gmail.com>
2018-03-10 19:41:26 +0300

    Binary_To_7Segment.v added + other changes

--- demos/uart_rx_tx/make.sh
+++ demos/uart_rx_tx/make.sh
@@ -4,7 +4,7 @@ set -e
 
 ./clean.sh
 
-yosys -p "synth_ice40 -blif main.blif" main.sv UART_TX.v
+yosys -p "read_verilog UART_RX.v; read_verilog UART_TX.v; read_verilog Binary_To_7Segment.v; read_verilog main.v; synth_ice40 -blif main.blif"
 arachne-pnr -d 1k -P vq100 -p main.pcf main.blif -o main.asc
 icetime -c 40 main.asc
 icepack main.asc main.bin
